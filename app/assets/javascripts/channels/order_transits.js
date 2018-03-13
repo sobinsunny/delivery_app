7b2d48e6c0e2a63fa15aca270dd1406cd1d0d940
Sobin <sobisunny@gmail.com>
2018-03-14 00:53:58 +0530

    Fixed uglifier issue

--- app/assets/javascripts/channels/order_transits.js
+++ app/assets/javascripts/channels/order_transits.js
@@ -2,9 +2,8 @@ App.messages = App.cable.subscriptions.create('OrdersChannel', {
   received: function(data) {
     // handling first update
     $("#no-content-label").hide();
-    return $(`#order_path-${data.order_id}`).append(this.renderMessage(data));
+    return $('#order_path-' + data.order_id).append(this.renderMessage(data));
   },
-
   renderMessage: function(data) {
     return "<tr><td>" + data.current_location + "</td><td>" + data.order_status + "</td><td>" + data.agent_name + "</td><td>" + data.agent_phone_number + "</td></tr>"
   }
